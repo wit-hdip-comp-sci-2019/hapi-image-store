561b649d59b9d8bbde1ebde165c6419160a3bf14
Eamonn de Leastar <edeleastar@wit.ie>
2020-02-24 14:58:46 +0000

    initialise public folder + inert route

--- routes.js
+++ routes.js
@@ -4,4 +4,14 @@ const Gallery = require('./app/controllers/gallery');
 
 module.exports = [
   { method: 'GET', path: '/', config: Gallery.index },
+  {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: './public'
+      }
+    },
+    options: { auth: false }
+  }
 ];
