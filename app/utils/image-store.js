68cd03ab784d7d8321c59389c9f137ef8c10c362
Eamonn de Leastar <edeleastar@wit.ie>
2020-02-24 14:47:30 +0000

    introduce utility to interact with cloudinary

---
+++ app/utils/image-store.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const cloudinary = require('cloudinary');
+
+const ImageStore = {
+  configure: function() {
+    const credentials = {
+      cloud_name: process.env.name,
+      api_key: process.env.key,
+      api_secret: process.env.secret
+    };
+    cloudinary.config(credentials);
+  },
+
+  getAllImages: async function() {
+    const result = await cloudinary.v2.api.resources();
+    return result.resources;
+  }
+};
+
+module.exports = ImageStore;
