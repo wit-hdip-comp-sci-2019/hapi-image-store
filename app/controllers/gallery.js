18575c44da270310cbae79cc547e8993510bfd8b
Eamonn de Leastar <edeleastar@wit.ie>
2020-02-24 14:49:36 +0000

    retrieve images from cloudinary account and display

--- app/controllers/gallery.js
+++ app/controllers/gallery.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const ImageStore = require('../utils/image-store');
+
 const Gallery = {
   index: {
     handler: async function(request, h) {
       try {
+        const allImages = await ImageStore.getAllImages();
         return h.view('gallery', {
-          title: 'Cloudinary Gallery'
+          title: 'Cloudinary Gallery',
+          images: allImages
         });
       } catch (err) {
         console.log(err);
@@ -15,4 +19,3 @@ const Gallery = {
 };
 
 module.exports = Gallery;
-
